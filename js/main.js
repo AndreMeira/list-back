05689eaba723837624bba246c5ff489fe57537bf
Andre Meira <a.meira92@gmail.com>
2015-08-27 10:53:50 +0200

    refs ...

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	window.onpopstate = function(event) {
 	  console.log('>>>>>>>>>>>>>>>>>>>> onpopstate', event);
 	  $('#main-region').html(ListBack.listHtml);
+	  $('#item-98')[0].scrollIntoView();
 	  //window.history.back();
 	};
 	var app = new Mn.Application({
